fb651efac8040b86ac33e20ef252353ef140efdc
rifayetazamtalukder <rifayetewucse@gmail.com>
2020-03-16 21:56:24 +0600

    Initial Commit for page-not-found, home-page, top-bar, side-bar component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    HomePageComponent,
+    TopBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
