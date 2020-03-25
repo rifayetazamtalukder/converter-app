5f1093e9cad0a6db371b276f03a359f5bd8fbd2a
rifayetazamtalukder <rifayetewucse@gmail.com>
2020-03-25 13:23:18 +0600

    Responsive topbar with sidenav added to the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,12 +33,38 @@ export class AppComponent {
       this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/developer_board-48dp.svg')
     );
     // 
-    // 
     this.iconRegistry.addSvgIcon(
       'developer-board-black-24',
       this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/developer_board-black-24dp.svg')
     );
-    // 
+    // menu white icon
+    this.iconRegistry.addSvgIcon(
+      'menu-white', this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/menu-white-48dp.svg')
+    );
+    // close white icon
+    this.iconRegistry.addSvgIcon(
+      'close-white', this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/close-white-48dp.svg')
+    );
+    // more_vert white icon
+    this.iconRegistry.addSvgIcon(
+      'more-vert-white', this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/more_vert-white-48dp.svg')
+    );
+    // Settings dark-gray icon
+    this.iconRegistry.addSvgIcon(
+      'settings-dark-gray', this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/settings-dark-gray-48dp.svg')
+    );
+    // about dark-gray icon
+    this.iconRegistry.addSvgIcon(
+      'announcement-dark-gray', this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/announcement-dark-gray-48dp.svg')
+    );
+    // Help dark-gray icon
+    this.iconRegistry.addSvgIcon(
+      'help-dark-gray', this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/help-dark-gray-48dp.svg')
+    );
+    // Person dark-gray icon
+    this.iconRegistry.addSvgIcon(
+      'person-dark-gray', this.sanitizer.bypassSecurityTrustResourceUrl('../assets/img/svg-icon/person-dark-gray-48dp.svg')
+    );
   }
 }
 
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent implements OnInit {
+export class TopBarComponent implements OnInit, OnDestroy {
+  // 
+  // Variable Lists
+  // 
+  private mediaType: Subscription;
+  public deviceType: string;
+  public navMode: string = 'over';
 
-  constructor() { }
 
-  ngOnInit(): void {
+  // 
+  constructor(private mediaObserver: MediaObserver) { }
+
+  // 
+  ngOnInit() {
+    // 
+    // This Will Call WHen The Component Will Initiate
+    // 
+    // Subscribe the mediaObserver Here
+    // 
+    this.mediaType = this.mediaObserver.media$.subscribe(
+      (change: MediaChange) => {
+        this.deviceType = change.mqAlias;
+
+        if (this.deviceType === 'xl' || this.deviceType === 'lg') {
+          this.navMode = 'side';
+        }
+        else {
+          this.navMode = 'over';
+        }
+      }
+    );
+  }
+
+
+  // 
+  ngOnDestroy() {
+    // 
+    // This will Call Whenever the page will destroy(User navigates to other components)
+    // 
+    // NOTE: Don't Unsubscribe the this.mediaType:
+    //      Because It Should Render all the time.
+    // 
   }
 
 }
